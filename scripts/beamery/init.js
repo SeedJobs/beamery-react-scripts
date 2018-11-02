e07bca32aae6ce490ed620722b05c39069dc9abb
Brody McKee <mrmckeb@users.noreply.github.com>
2018-11-02 17:29:51 +0200

    Update scripts and add linting (#10)

--- scripts/beamery/init.js
+++ scripts/beamery/init.js
@@ -1,9 +1,64 @@
 'use strict';
 
+const fs = require('fs-extra');
+const path = require('path');
+const os = require('os');
+const spawn = require('react-dev-utils/crossSpawn');
 const getDependencies = require('./dependencies');
 
-module.exports = function(appPackage, args, command, spawn, useTypeScript) {
-  // Set up dependencies.
+const SCRIPT_GLOB = './src/**/*.[j|t]s?(x)';
+const STYLE_GLOB = './src/**/*.?(s)css';
+
+module.exports = function(appPath, args, command, useTypeScript) {
+  const appPackage = require(path.join(appPath, 'package.json'));
+
+  appPackage.scripts = {
+    start: 'BMR_ENV=development react-scripts start',
+    'start-independent': 'react-scripts start',
+    storybook: 'start-storybook -p 3001',
+    build: 'BMR_ENV=production react-scripts build',
+    'build-dev': 'BMR_ENV=development react-scripts build',
+    'build-staging': 'BMR_ENV=staging react-scripts build',
+    'build-storybook': 'build-storybook',
+    lint: 'yarn lint-scss && yarn lint-scripts',
+    'lint-scripts': `eslint '${SCRIPT_GLOB}'`,
+    'lint-scss': `stylelint '${STYLE_GLOB}'`,
+    test: 'react-scripts test --colors',
+    'test-coverage': 'yarn test --coverage',
+    'test-watch': 'yarn test --watch',
+  };
+
+  appPackage.stylelint = {
+    extends: 'stylelint-config-standard',
+  };
+
+  appPackage.jest = {
+    snapshotSerializers: ['enzyme-to-json/serializer'],
+    collectCoverageFrom: [
+      'src/**/*.{js,jsx,ts,tsx}',
+      '!src/**/*.stories.*',
+      '!src/**/(serviceWorker|setupTests).*',
+    ],
+  };
+
+  appPackage['lint-staged'] = {
+    [STYLE_GLOB]: ['yarn lint-scss --fix', 'git add'],
+    [SCRIPT_GLOB]: ['yarn lint-scripts --fix --quiet', 'git add'],
+  };
+
+  appPackage.husky = {
+    hooks: {
+      'pre-commit': 'lint-staged',
+      'pre-push': 'yarn test',
+    },
+  };
+
+  fs.writeFileSync(
+    path.join(appPath, 'package.json'),
+    JSON.stringify(appPackage, null, 2) + os.EOL
+  );
+
+  // Set up dependencies. This must be done after the above.
   function addDependencies(depArr, isDev) {
     console.log(`Installing Beamery dependencies using ${command}...`);
     console.log();
--- scripts/utils/verifyTypeScriptSetup.js
+++ scripts/utils/verifyTypeScriptSetup.js
@@ -16,6 +16,7 @@ const paths = require('../../config/paths');
 const os = require('os');
 const immer = require('react-dev-utils/immer').produce;
 const globby = require('react-dev-utils/globby').sync;
+const beameryOptions = require('../beamery/typescript');
 
 function writeJson(fileName, object) {
   fs.writeFileSync(fileName, JSON.stringify(object, null, 2) + os.EOL);
@@ -85,7 +86,7 @@ function verifyTypeScriptSetup() {
     process.exit(1);
   }
 
-  const compilerOptions = {
+  const compilerOptions = beameryOptions({
     // These are suggested values and will be set when not present in the
     // tsconfig.json
     // 'parsedValue' matches the output value from ts.parseJsonConfigFileContent()
@@ -128,7 +129,7 @@ function verifyTypeScriptSetup() {
       reason: 'absolute imports are not supported (yet)',
     },
     paths: { value: undefined, reason: 'aliased imports are not supported' },
-  };
+  });
 
   const formatDiagnosticHost = {
     getCanonicalFileName: fileName => fileName,
@@ -208,8 +209,9 @@ function verifyTypeScriptSetup() {
       messages.push(
         `${coloredOption} ${chalk.bold(
           valueToCheck == null ? 'must not' : 'must'
-        )} be ${valueToCheck == null ? 'set' : chalk.cyan.bold(value)}` +
-          (reason != null ? ` (${reason})` : '')
+        )} be ${
+          valueToCheck == null ? 'set' : chalk.cyan.bold(JSON.stringify(value))
+        }` + (reason != null ? ` (${reason})` : '')
       );
     }
   }
@@ -221,17 +223,6 @@ function verifyTypeScriptSetup() {
       `${chalk.cyan('include')} should be ${chalk.cyan.bold('src')}`
     );
   }
-
-  // bmr-react-scripts start
-  if (parsedTsConfig.compilerOptions.baseUrl == null) {
-    appTsConfig.baseUrl = '.';
-  }
-  if (parsedTsConfig.compilerOptions.paths == null) {
-    appTsConfig.paths = {
-      '*': ['src/*'],
-    };
-  }
-  // bmr-react-scripts end
 
   if (messages.length > 0) {
     if (firstTimeSetup) {
